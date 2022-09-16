fb14f6997d0f67871302777e8ab8b1f320aea243
haiyuan.yu <haiyuan.yu@thoughtworks.com>
2022-09-16 21:00:05 +0800

    [haiyuan yu & zheyu shen] #N/A feat: add task and rename test

--- src/test/java/com/example/AEPB/AepbApplicationTests.java
+++ src/test/java/com/example/AEPB/AepbApplicationTests.java
@@ -1,11 +1,10 @@
 package com.example.AEPB;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class HelloTest {
+class ParkingLotTest {
 
 	@Test
 	void should_no_null() {
